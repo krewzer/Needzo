0bf34fd87a0bda326e1d1c722469897f2f1f99df
Atishay <xatishayx@pop-os.localdomain>
2020-06-20 22:06:37 +0530

    added requestdelivery modal

--- frontend/src/components/Hmap.js
+++ frontend/src/components/Hmap.js
@@ -1,9 +1,27 @@
 // src/DisplayMapFC.js
 
-import React from "react";
+import React, { useState } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap";
+import "../App.css";
 
 export const HMap = (props) => {
   const mapRef = React.useRef(null);
+
+  const [modal, setModal] = useState(false);
+  const [details, setDetails] = useState({});
+
+
+  const toggle = () => setModal(!modal);
+
   React.useLayoutEffect(() => {
     if (!mapRef.current) return;
     const H = window.H;
@@ -31,18 +49,30 @@ export const HMap = (props) => {
             let item = result.items[0];
             const currentGroup = new H.map.Group();
             map.addObject(currentGroup);
-            console.log(item);
+            // console.log(item);
             map.setCenter(item.position);
             const currentMarker = new H.map.Marker(item.position);
-            console.log(item);
+            // console.log(item);
             // currentMarker.setData(ele);
             currentGroup.addObject(currentMarker);
-            // currentGroup.addEventListener("tap", (evt) => {});
+            currentGroup.addEventListener(
+              "tap",
+               async () => {
+                await setDetails({details: ele});
+                console.log(ele)
+                console.log(details)
+                setModal(!modal)
+                
+              }
+            );
+
+
           },
           alert
         )
       );
     };
+  
     console.log(props.data);
     addMarkerfromData(platform, props.data);
 
@@ -50,6 +80,54 @@ export const HMap = (props) => {
       map.dispose();
     };
   }, [mapRef, props.data]);
-
-  return <div className="map" ref={mapRef} style={{ height: "500px" }} />;
-};
+  
+  return (
+  <div>
+  <div className="map" ref={mapRef} style={{ height: "500px" }} />
+  <Modal
+              isOpen={modal}
+              toggle={toggle}
+              contentClassName="custom-modal-style"
+              labelledBy="request"
+            >
+              <ModalHeader toggle={toggle}>
+                Help others by Volunteering
+              </ModalHeader>
+              <ModalBody>
+                <Container className="text-left">
+                  <Row>
+                    <Col xs="12" sm="6">
+                      <br />
+                      <h3> Deliver To :</h3>
+                      <br />
+                      
+                      <br />
+                      <h3>Details :</h3>
+                      <br />
+                      <br />
+                      <h3>Address :</h3>
+                    </Col>
+                    <Col xs="12" sm="6">
+                      <img
+                        src={require("../assets/groceries.png")}
+                        alt="logo"
+                        height="90%"
+                        width="100%"
+                        style={{ marginLeft: 20 }}
+                      />
+                      <center>
+                        <h3> Groceries</h3>
+                      </center>
+                    </Col>
+                  </Row>
+                </Container>
+              </ModalBody>
+              <ModalFooter>
+                
+                <Button color="danger" onClick={toggle}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </Modal>
+    </div>
+  )};
--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,77 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
-import '../sidebar.css'
-import { Button } from "reactstrap";
-const Dashboard = () => (
-  <div className="sidebar-container">
-    <div className="sidebar-inner-container">
-      <div className="sidebar-container-header">
-        <div className="sidebar-container-inner-header">
-          <div
-            className="sidebar-container-header-available"
-            onClick={(e) => {
-              e.preventDefault();
-          //    clickHandler("available");
-            }}
-          >
-            <div
-              // className={
-              //   available
-              //     ? "available-title-name-active"
-              //     : "available-title-name"
-              // }
-            >
-              Delivery Requests
-            </div>
-          </div>
-          <div
-            className="sidebar-container-header-active"
-            onClick={(e) => {
-              e.preventDefault();
-           //   clickHandler("active");
-            }}
-          >
-          
-          </div>
-        </div>
-      </div>
-      <div className="sidebar-container-inner-body" id="scroll-container">
-        {/* {available ? (
-          <AvailableSidebar
-            available={helpNeededTasks}
-            updateTask={updateTask}
-            openModal={openModal}
-            closeModal={closeModal}
-            currentUserId={currentUserId}
-            history={history}
-            activeTask={activeTask}
-            receiveActiveTaskId={props.receiveActiveTaskId}
-            currentPosition={userLocation}
-            receiveNewTask={receiveNewTask}
-          />
-        ) : (
-          <ActiveSidebar
-            session={session}
-            active={currentUserTasks}
-            updateTask={updateTask}
-            openModal={openModal}
-            closeModal={closeModal}
-            currentUserId={currentUserId}
-            history={history}
-            activeTask={activeTask}
-            receiveActiveTaskId={props.receiveActiveTaskId}
-            currentPosition={userLocation}
-            receiveNewTask={receiveNewTask}
-          />
-        )} */}
-      </div>
-    </div>
-    <Button color="info">Click me</Button>
-  </div>
-);
-=======
 import { HMap } from "../components/Hmap";
-
+import {Button } from "reactstrap";
+import RequestCardModal from "../components/RequestcardModal";
 const Dashboard = () => {
   const API_URL = "http://localhost:5000/api/delivery";
   const [data, setData] = React.useState([]);
@@ -88,9 +18,9 @@ const Dashboard = () => {
   return (
     <div className="bg-light p-3 text-center">
       <HMap data={data} />
+      <RequestCardModal />  
     </div>
   );
 };
->>>>>>> b500c4163cdd1aa687523c734a50575271b90f74
 
 export default Dashboard;
