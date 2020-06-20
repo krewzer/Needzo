e955a19b948cd38a48d54d0c462e33dd3490b86a
Atishay <xatishayx@pop-os.localdomain>
2020-06-20 19:28:21 +0530

    added pngs

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,8 +1,71 @@
 import React from "react";
-
+import '../sidebar.css'
+import { Button } from "reactstrap";
 const Dashboard = () => (
-  <div className="bg-light p-3 text-center">
-    <p>Hello</p>
+  <div className="sidebar-container">
+    <div className="sidebar-inner-container">
+      <div className="sidebar-container-header">
+        <div className="sidebar-container-inner-header">
+          <div
+            className="sidebar-container-header-available"
+            onClick={(e) => {
+              e.preventDefault();
+          //    clickHandler("available");
+            }}
+          >
+            <div
+              // className={
+              //   available
+              //     ? "available-title-name-active"
+              //     : "available-title-name"
+              // }
+            >
+              Delivery Requests
+            </div>
+          </div>
+          <div
+            className="sidebar-container-header-active"
+            onClick={(e) => {
+              e.preventDefault();
+           //   clickHandler("active");
+            }}
+          >
+          
+          </div>
+        </div>
+      </div>
+      <div className="sidebar-container-inner-body" id="scroll-container">
+        {/* {available ? (
+          <AvailableSidebar
+            available={helpNeededTasks}
+            updateTask={updateTask}
+            openModal={openModal}
+            closeModal={closeModal}
+            currentUserId={currentUserId}
+            history={history}
+            activeTask={activeTask}
+            receiveActiveTaskId={props.receiveActiveTaskId}
+            currentPosition={userLocation}
+            receiveNewTask={receiveNewTask}
+          />
+        ) : (
+          <ActiveSidebar
+            session={session}
+            active={currentUserTasks}
+            updateTask={updateTask}
+            openModal={openModal}
+            closeModal={closeModal}
+            currentUserId={currentUserId}
+            history={history}
+            activeTask={activeTask}
+            receiveActiveTaskId={props.receiveActiveTaskId}
+            currentPosition={userLocation}
+            receiveNewTask={receiveNewTask}
+          />
+        )} */}
+      </div>
+    </div>
+    <Button color="info">Click me</Button>
   </div>
 );
 
